4c7e79e1909506afbf137c7096e8ffe1938c8c87
tranluan91 <tranluan91@gmail.com>
2013-07-24 10:45:28 +0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  get "users/new"
+
+	root  'static_pages#home'
+	match '/signup',	to: 'users#new',						via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
 end
